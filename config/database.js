d182ca47ee37d824dc17f195bd293a74aed442e9
Aman Aryan <amanaryan33736@gmail.com>
2025-03-27 12:43:21 +0530

    updating dependencies version using npm

--- config/database.js
+++ config/database.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDatabase = async () => {
     try {
-        const Connect = await mongoose.connect(process.env.MONGO_URI);
+        const Connect = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log(`MongoDB Connected`);
     } catch (error) {
         console.log(`Error: ${error.message}`);
